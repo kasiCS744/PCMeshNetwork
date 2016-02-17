b9f11397d7185207d1e9d62f3f91e0a6e4dc0e39
henryhalling <henryhalling@yahoo.com>
2016-02-17 05:04:43 -0600

    Made a lot of GUI changes

---
+++ cs744/ser/checkLogin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liu
+ * Date: 2016/2/8
+ * Time: 22:45
+ */
+include_once "dao/DBHelper.php";
+include "css/bootstrap.min.css";
+$username=$_POST['username'];
+$password=$_POST['password'];
+if(isUsernameAndPasswordMatches($username,$password)){
+   // echo 1;
+    session_start();
+    $_SESSION['uid']=getUidByUsername($username);
+    header("location:answerSecurityQuestion.php");
+}else{
+    header("location:Login.html");
+}
+
+
+function getUidByUsername($username){
+    $sql="select uid from user where username='".$username."'";
+    return mysql_fetch_array(mysql_query($sql))[0];
+}
+function isUsernameAndPasswordMatches($username,$password)
+{
+    $sql = "select * from user where username='" . $username . "' and password='" . $password . "'";
+   // echo $sql;
+    $result = mysql_query($sql);
+    if (mysql_fetch_array($result) == null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+?>
--- cs744/ser/logout.php
+++ cs744/ser/logout.php
@@ -4,7 +4,8 @@
  * User: Liu
  * Date: 2016/2/9
  * Time: 10:56
- */session_start();
+ */
+session_start();
 session_destroy();
 header("location:../Login.html");
 ?>
